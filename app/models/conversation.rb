9f69dd6730b9e299555a87b6d4d49309326f6e75
Roman <broilerster@gmail.com>
2012-05-10 12:51:04 +0300

    Added chronological ordering of messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,7 +2,7 @@ class Conversation < ActiveRecord::Base
   attr_protected
   attr_accessor :reader, :mailbox
 
-  has_many :messages, inverse_of: :conversation
+  has_many :messages, inverse_of: :conversation, order: 'messages.created_at ASC'
   has_many :receipts, through: :messages
 
   def messages(*)
