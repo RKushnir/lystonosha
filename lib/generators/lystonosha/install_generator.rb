3a4d6cc1e76323f33c45b68f31de13ebf6306e2b
Roman <broilerster@gmail.com>
2012-05-14 17:03:59 +0300

    Added a template for initializer.

--- lib/generators/lystonosha/install_generator.rb
+++ lib/generators/lystonosha/install_generator.rb
@@ -11,6 +11,11 @@ module Lystonosha
         ActiveRecord::Migration.next_migration_number(next_migration_number)
       end
 
+      desc "Creates a Lystonosha initializer."
+      def copy_initializer
+        template "lystonosha.rb", "config/initializers/lystonosha.rb"
+      end
+
       desc "Creates a migration for Lystonosha models."
       def copy_lystonosha_migration
         migration_template "migration.rb", "db/migrate/create_lystonosha"
---
+++ lib/generators/templates/lystonosha.rb
@@ -0,0 +1,4 @@
+Lystonosha.setup do |config|
+  # Provide a callback you wish to be called after message has been delivered.
+  # config.message_delivered = ->(message) {}
+end
